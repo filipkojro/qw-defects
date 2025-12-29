c41f88592236a17af6beb2f1d780df4df3236b6c
Filip Kojro <filiopeq@gmail.com>
2025-12-29 17:35:34 +0100

    walk function with start state

--- walk.py
+++ walk.py
@@ -11,13 +11,25 @@ def control_add_one(position_qubits: int) -> QuantumCircuit:
     return qc
 
 class walk_circuit_simpler:
-    def __init__(self, nodes_power, num_steps, use_phase_defect=False, defect_step=None, defect_qubit=None, defect_strength=None):
+    def __init__(self, nodes_power, num_steps, start_bits= None, start_phases = None, use_phase_defect=False, defect_step=None, defect_qubit=None, defect_strength=None):
         self.nodes_power = nodes_power
         self.num_steps = num_steps
         self.use_phase_defect = use_phase_defect
         self.defect_step = defect_step
         self.defect_qubit = defect_qubit
         self.defect_strength = defect_strength
+
+        if start_bits is not None:
+            self.start_bits = list(reversed(start_bits))
+            assert len(self.start_bits) == self.nodes_power, f"length of start_bits must be the same as nodes_power, got: {len(self.start_bits)} expected: {self.nodes_power}"
+        else:
+            self.start_bits = "0" * self.nodes_power
+
+        if start_phases is not None:
+            self.start_phases = list(reversed(start_phases))
+            assert len(self.start_phases) == self.nodes_power, f"length of start_phases must be the same as nodes_power, got: {len(self.start_phases)} expected: {self.nodes_power}"
+        else:
+            self.start_phases = [0] * 2**self.nodes_power
 
     def build(self) -> QuantumCircuit:
         q_pos = QuantumRegister(self.nodes_power, "q_pos")
@@ -27,7 +39,12 @@ class walk_circuit_simpler:
 
         qc = QuantumCircuit(*regs)
 
-        qc.x(q_pos[-1])
+        # setting start state
+        for i, bit in enumerate(self.start_bits):
+            if bit == "1":
+                qc.x(q_pos[i])
+            
+            qc.p(self.start_phases[i], q_pos[i])
 
         for step in range(self.num_steps):
             # coin
